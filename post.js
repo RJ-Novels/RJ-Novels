2099d05ffd34e5f76a6cb52f3b5e9937e68ddd77
RJ-Novels <yaghshsh25@gmail.com>
2026-01-05 20:35:18 +0600

    Delete post.js

--- post.js
+++
@@ -1,9 +0,0 @@
-  const btn = document.getElementById("readMoreBtn");
-  const text = document.getElementById("descText");
-
-  btn.addEventListener("click", () => {
-    text.classList.toggle("expanded");
-    btn.textContent = text.classList.contains("expanded")
-      ? "Read Less"
-      : "Read More";
-  });
